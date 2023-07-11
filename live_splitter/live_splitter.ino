3a8b9e1cb385ee82ac93e956f3b33d7bab49b60a
kannadan <joonas.sou@gmail.com>
2023-07-11 17:13:16 +0300

    fixed none functioning code

--- live_splitter/live_splitter.ino
+++ live_splitter/live_splitter.ino
@@ -3,21 +3,23 @@
 const int buttonPin = 10;
 int buttonState = HIGH;
 int previousButtonState = HIGH;
-
+const int debounceDelay = 50;  
+int lastDebounceTime = millis();  
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP);
   Keyboard.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
   buttonState = digitalRead(buttonPin);
-  if (buttonState != previousButtonState) {
-    Serial.println("Button pressed1!");
-    if (buttonState == LOW) {
-      Keyboard.press(KEY_F20);
-      delay(50); // Adjust the delay if needed
-      Keyboard.release(KEY_F20);
-    }
-    previousButtonState = buttonState;
+  if (buttonState == LOW && previousButtonState == HIGH && millis() - lastDebounceTime > debounceDelay) {
+    lastDebounceTime = millis(); 
+    Serial.println("Button press");
+    Keyboard.press(KEY_F20);
+    delay(50); // Adjust the delay if needed
+    Keyboard.release(KEY_F20);
+    delay(550);
   }
+  previousButtonState = buttonState;
 }
